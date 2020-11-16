7929b2c951c8351d3498056e7a3c448816dacc31
cloudsample6 <cloudsample6@gmail.com>
2020-11-17 01:47:55 +0700

    change route

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::livewire('/', 'shop.index')
+->name('index');
 
 Auth::routes();
 
